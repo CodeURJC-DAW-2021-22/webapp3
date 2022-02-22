3687042fb6bf125bf4d85d2b673b3b502f1186d9
AndyNuzzi <andy.nuzzi@gmail.com>
2022-02-22 10:22:41 +0100

    extract header y footer

--- backend/src/main/java/webapp3/webapp3/MemberController.java
+++ backend/src/main/java/webapp3/webapp3/MemberController.java
@@ -1,5 +1,6 @@
 package webapp3.webapp3;
 
+import antlr.ASTNULLType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 
-    @GetMapping("/socio")
+    @GetMapping("/exercise")
     public String memberPage(Model model){
         return "USRMEM_01ExerciseTable";
+    }
+
+    @GetMapping("/editProfile")
+    public String editProfile(Model model) {
+        return "USRMEM_02EditProfile";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        return "USRMEM_02Profile";
+    }
+
+    @GetMapping("/statistics")
+    public String statistics(Model model) {
+        return "USRMEM_03Estatistics";
+    }
+
+    @GetMapping("/activities")
+    public String activities(Model model) {
+        return "USRMEM_04Activities";
     }
 }
