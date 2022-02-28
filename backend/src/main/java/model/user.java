bc6a7d5de978d466bf75316cd7635c2d1fce3d62
ruky2 <c.fernandezl.2019@alumnos.urjc.es>
2022-02-28 12:01:24 +0100

    User entity.

---
+++ backend/src/main/java/model/user.java
@@ -0,0 +1,51 @@
+package model;
+import org.hibernate.type.DateType;
+import org.hibernate.type.LocalDateTimeType;
+
+import java.sql.Blob;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class user {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String NIF;
+    private String email;
+    private String password;
+    private String address;
+    private String postalCode;
+    private DateType birthday;
+    private String phone;
+    @Lob
+    private Blob imageFile;
+    private boolean image;
+
+    private enum Demarcacion {member,admministrator,monitor}
+    @ManyToMany
+    private Demarcacion  userType;
+
+    private DateType entryDate;
+    private int height;
+    private int weight;
+    private int IBAN;
+    private String medicalInfo;
+    private LocalDate sickLeave;
+    private DateType hiringDate;
+
+
+
+
+
+
+}
