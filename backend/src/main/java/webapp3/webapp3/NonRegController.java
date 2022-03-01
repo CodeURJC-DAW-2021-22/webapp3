350ec0ff249e8853b0d04b8078b35f8f8d4d9087
nestorgranado <nestor2903@gmail.com>
2022-03-01 10:13:08 +0100

    update files

--- backend/src/main/java/webapp3/webapp3/NonRegController.java
+++ backend/src/main/java/webapp3/webapp3/NonRegController.java
@@ -5,17 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Controller
@@ -138,14 +137,15 @@ public class NonRegController {
 
     //Sign in page
     @GetMapping("/USR_sign_in")
-    public String signin(Model model, @RequestParam String name, @RequestParam String surname, @RequestParam String NIF, @RequestParam DateType birthday,
-                         @RequestParam String phone_num, @RequestParam String postal_code, @RequestParam String address, @RequestParam String email,
-                         @RequestParam String password, @RequestParam DateType entryDate, @RequestParam int height, @RequestParam int weight,
-                         @RequestParam String IBAN, @RequestParam String medicalInfo){
+    public String signin(Model model){return "USR_07sign_in";}
 
+    @PostMapping("/USR_sign_in")
+    public String postSignin(Model model,@RequestParam String name, @RequestParam String surname, @RequestParam String NIF, @RequestParam DateType birthday,
+                             @RequestParam String phone_num, @RequestParam String postal_code, @RequestParam String address, @RequestParam String email,
+                             @RequestParam String password, @RequestParam DateType entryDate, @RequestParam int height, @RequestParam int weight,
+                             @RequestParam String IBAN, @RequestParam String medicalInfo){
 
-
-        return "USR_07sign_in";
+        return "redirect:/USR_log_in";
     }
 
 }
