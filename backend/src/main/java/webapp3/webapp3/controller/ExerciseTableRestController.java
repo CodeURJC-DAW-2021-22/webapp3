bae8c51057b3361ad13c3e04c5f1e1d57137ea26
AndyNuzzi <andy.nuzzi@gmail.com>
2022-03-24 20:48:32 +0100

    adding image management to exercise table API REST

--- backend/src/main/java/webapp3/webapp3/controller/ExerciseTableRestController.java
+++ backend/src/main/java/webapp3/webapp3/controller/ExerciseTableRestController.java
@@ -2,10 +2,14 @@ package webapp3.webapp3.controller;
 
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import webapp3.webapp3.model.ExerciseTable;
 import webapp3.webapp3.model.User;
 import webapp3.webapp3.service.ExerciseTableService;
@@ -13,9 +17,13 @@ import webapp3.webapp3.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
 @RequestMapping("/api/exercises-tables")
@@ -90,6 +98,39 @@ public class ExerciseTableRestController {
     }
     //validations error
 
-    //management images (add[post], modify[put] y get[get])
+    @GetMapping("/{id}/image")
+    public ResponseEntity<Object> downloadImage(@PathVariable long id) throws SQLException {
+
+        ExerciseTable exerTab = exerTabServ.findById(id).orElseThrow();
+
+        if (exerTab.getImage() != null) {
+
+            Resource file = new InputStreamResource(exerTab.getImage().getBinaryStream());
+
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
+                    .contentLength(exerTab.getImage().length()).body(file);
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}/image")
+    // this method is a PUT because uploading an image in API REST is a form-data type, not a JSON.
+    // I can't create an exercise table and introduce an image in the same petition
+    public ResponseEntity<Object> uploadImage(@PathVariable long id, @RequestParam MultipartFile imageFile)
+            throws IOException {
+
+        ExerciseTable exerTab = exerTabServ.findById(id).orElseThrow();
+
+        URI location = fromCurrentRequest().build().toUri(); // brings URI from the actual get
+
+        exerTab.hasImage();
+        exerTab.setImage(BlobProxy.generateProxy(imageFile.getInputStream(), imageFile.getSize()));
+        exerTabServ.save(exerTab);
+
+        return ResponseEntity.created(location).build(); // brings url from the image
+        // created code is 201
+    }
 
 }
