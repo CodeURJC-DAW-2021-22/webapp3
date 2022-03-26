2c924c3238304d15a6054e4552ca3a74628dbbba
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2022-03-26 13:10:45 +0100

    User Rest Controller PUT.

--- backend/src/main/java/webapp3/webapp3/controller/UserRestController.java
+++ backend/src/main/java/webapp3/webapp3/controller/UserRestController.java
@@ -107,6 +107,31 @@ public class UserRestController {
         return ResponseEntity.created(location).build();
     }
 
+    //PUT monitor
+    @PutMapping("/monitors/{id}/")
+    public ResponseEntity<User> updateMonitor(@PathVariable long id, @RequestBody User updatedUser) throws SQLException {
+        if (usrServ.exist(id)) {
+            if(usrServ.findById(id).get().getUserType().equals("monitor") && updatedUser.getUserType().equals("monitor")){
+                if (updatedUser.getImage() != null) {
+                    User dbUser = usrServ.findById(id).orElseThrow();
+                    if (dbUser.getImage() != null) {
+                        updatedUser.setImage(BlobProxy.generateProxy(dbUser.getImage().getBinaryStream(),
+                                dbUser.getImage().length()));
+                    }
+                }
+
+                updatedUser.setId(id);
+                usrServ.save(updatedUser);
+
+                return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } else	{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 
 
 
