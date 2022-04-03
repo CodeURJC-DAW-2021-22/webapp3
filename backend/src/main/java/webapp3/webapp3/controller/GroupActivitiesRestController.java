44922edb5b1c4e6bef9e38f7d191161cda25c97c
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2022-04-03 11:37:35 +0200

    GroupActivitiesRestController api docs update

--- backend/src/main/java/webapp3/webapp3/controller/GroupActivitiesRestController.java
+++ backend/src/main/java/webapp3/webapp3/controller/GroupActivitiesRestController.java
@@ -1,5 +1,10 @@
 package webapp3.webapp3.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import webapp3.webapp3.model.Activity;
 import webapp3.webapp3.model.ExerciseTable;
+import webapp3.webapp3.model.User;
 import webapp3.webapp3.service.ActivityService;
 
 import java.io.IOException;
@@ -29,10 +35,53 @@ public class GroupActivitiesRestController {
     @Autowired
     private ActivityService actServ;
 
+    //GET group activities
+    @Operation(summary = "Get all group activities")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the group activity",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not found",
+                    content = @Content
+            )
+    })
+
     @GetMapping("/")
     public ResponseEntity<List<Activity>> getGroupActivities(){
         return ResponseEntity.ok(actServ.findAll());
     }
+
+    //GET group activity with id
+    @Operation(summary = "Get a group activity by id")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the group activity",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation=Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "group activity not found",
+                    content = @Content
+            )
+    })
 
     @GetMapping("/{id}")
     public ResponseEntity<Activity> getGroupActivity(@PathVariable Long id){
@@ -44,6 +93,30 @@ public class GroupActivitiesRestController {
         }
     }
 
+    //POST group activity
+    @Operation(summary = "Post a group activity")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Created",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation=Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "group activity not found",
+                    content = @Content
+            )
+    })
+
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
@@ -51,6 +124,35 @@ public class GroupActivitiesRestController {
         actServ.save(activity);
         return ResponseEntity.created(location).build();
     }
+
+    //PUT group activity
+    @Operation(summary = "PUT group activity")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "group activity found",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation=Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "group activity not found",
+                    content = @Content
+            )
+    })
 
     @PutMapping("/{id}")
     public ResponseEntity<Activity> updateActivity(@PathVariable long id, @RequestBody Activity updatedAct) throws SQLException {
@@ -72,6 +174,35 @@ public class GroupActivitiesRestController {
         }
     }
 
+    //DELETE group activity
+    @Operation(summary = "Delete group activity")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "group activity delete",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation=Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "group activity not found",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            )
+    })
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Activity> deleteGroupActivities(@PathVariable long id) {
         try {
@@ -82,6 +213,30 @@ public class GroupActivitiesRestController {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
+
+    //GET group activity image
+    @Operation(summary = "Get a group activity image")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the group activity",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation=Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Image not found",
+                    content = @Content
+            )
+    })
 
     @GetMapping("/{id}/image")
     public ResponseEntity<Object> downloadImage(@PathVariable long id) throws SQLException {
@@ -99,6 +254,30 @@ public class GroupActivitiesRestController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    //PUT group activity image
+    @Operation(summary = "PUT a group activity image")
+
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Created",
+                    content = {@Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation=Activity.class)
+                    )}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid id supplied",
+                    content = @Content
+            )
+    })
 
     @PutMapping("/{id}/image")
     // this method is a PUT because uploading an image in API REST is a form-data type, not a JSON.
