fd24b4c2c61286408c52e8964bd389d6147f5865
AlessandroNuzziURJC <80722183+AlessandroNuzziURJC@users.noreply.github.com>
2022-04-04 10:11:49 +0200

    Update deployApp.sh

--- docker/deployApp.sh
+++ docker/deployApp.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# $1 = alessandronuzziurjc/codeurjc-daw-2021-22-webapp3
+# $2 = codeurjc-daw-2021-22-webapp3
 
-docker pull alessandronuzziurjc/codeurjc-daw-2021-22-webapp3
+docker pull $1 
 
-docker tag alessandronuzziurjc/codeurjc-daw-2021-22-webapp3 registry.heroku.com/codeurjc-daw-2021-22-webapp3/web
+docker tag $1 registry.heroku.com/$2/web
 
-docker push registry.heroku.com/codeurjc-daw-2021-22-webapp3/web
+docker push registry.heroku.com/$2/web
 
-heroku container:release web -a codeurjc-daw-2021-22-webapp3
+heroku container:release web -a $2
 
-heroku logs --tail -a codeurjc-daw-2021-22-webapp3
+heroku logs --tail -a $2
