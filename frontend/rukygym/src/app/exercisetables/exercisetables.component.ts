697752a4215ab39e2c9bfe474c1b38eeb8f39a7e
AndyNuzzi <andy.nuzzi@gmail.com>
2022-04-25 12:31:05 +0200

    adding exercise tables component

---
+++ frontend/rukygym/src/app/exercisetables/exercisetables.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exercisetables',
+  templateUrl: './exercisetables.component.html',
+  styleUrls: ['./exercisetables.component.css']
+})
+export class ExercisetablesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
