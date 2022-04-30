9637608d9969c659400c2f0fc65090b8fa903ced
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2022-04-30 16:45:23 +0200

    app routing

--- frontend/rukygym/src/app/app.module.ts
+++ frontend/rukygym/src/app/app.module.ts
@@ -1,43 +1,39 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { routing } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+
 import { FooterComponent } from './components/footer/footer.component';
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 import { ContentComponent } from './components/content/content.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
-import { ActivitiesComponent } from './components/activities/activities.component';
-import { ExercisetablesComponent } from './components/exercisetables/exercisetables.component';
-import { StatisticsComponent } from './components/statistics/statistics.component'
-import { routing } from './app.routing';
+import { MonitorActivities } from './components/monitorActivities/monitorActivities.component';
+import { ActivityInfo } from './components/activityInfo/activityInfo.component';
+import { MonitorSchedule } from './components/monitorSchedule/monitorSchedule.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     NavbarComponent,
     SidemenuComponent,
     ContentComponent,
-    ProfileComponent,
-    EditProfileComponent,
-    ActivitiesComponent,
-    ExercisetablesComponent,
-    StatisticsComponent,
+    FooterComponent,
+    MonitorActivities,
+    ActivityInfo,
+    MonitorSchedule,
   ],
-
   imports: [
     BrowserModule,
     FormsModule,
     NgbModule,
     routing,
-    CommonModule,
+    HttpClientModule,
   ],
-
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/rukygym/src/app/app.routing.ts
+++ frontend/rukygym/src/app/app.routing.ts
@@ -5,18 +5,16 @@ import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContentComponent } from './components/content/content.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ActivitiesComponent } from './components/activities/activities.component';
-import { ExercisetablesComponent } from './components/exercisetables/exercisetables.component'
-import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { StatisticsComponent } from './components/statistics/statistics.component';
+import { ActivityInfo } from './components/activityInfo/activityInfo.component';
+import { MonitorActivities } from './components/monitorActivities/monitorActivities.component';
+import { MonitorSchedule } from './components/monitorSchedule/monitorSchedule.component';
+
+
 
 const appRoutes = [
-    {path: 'new/activities', component: ActivitiesComponent},
-    {path: 'new/exercise-tables', component: ExercisetablesComponent},
-    {path: 'new/profile/edit/:id', component: EditProfileComponent},
-    {path: 'new/profile', component: ProfileComponent},
-    {path: 'new/statistics', component: StatisticsComponent},
+    {path: 'new/activities', component: MonitorActivities},
+    {path: 'new/activity/:id', component: ActivityInfo},
+    {path: 'new/schedule', component: MonitorSchedule},
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);
