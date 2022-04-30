a3ffe5582c33f5bcbc092e9d95f15a30343da6e7
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2022-04-30 16:44:23 +0200

    Side menu, navbar content and footer components

---
+++ frontend/rukygym/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+
+}
+
