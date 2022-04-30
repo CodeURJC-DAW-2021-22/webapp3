5748682f7090a3574f42feaa8a962b8a4c6ba0da
nestorgranado <nestor2903@gmail.com>
2022-05-01 00:45:47 +0200

    connection beetwen activities, schedule and api

--- frontend/rukygym/src/app/components/activities/activities.component.ts
+++ frontend/rukygym/src/app/components/activities/activities.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
 
+import { Router, ActivatedRoute } from "@angular/router";
+import { Activity } from "../../models/Activity.model";
+
+import { ActivityService } from "../../services/Activity.service";
 
 @Component({
     selector:'activities',
@@ -7,5 +11,18 @@ import { Component } from "@angular/core";
     styleUrls: ['./activities.component.css']
 })
 export class ActivitiesComponent {
+
+  activities : Activity [] | undefined;
+
+    constructor(private router: Router, activatedRoute: ActivatedRoute, public service: ActivityService,
+        /*public loginService: LoginService*/) {
+
+        service.getActivities().subscribe(
+            activities  => this.activities = activities as Activity [],
+            (error: any)                => console.error(error),
+        );
+
+        console.log(this.activities);
+    }
 
 }
--- frontend/rukygym/src/app/components/schedule/schedule.component.ts
+++ frontend/rukygym/src/app/components/schedule/schedule.component.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
 
+import { ActivatedRoute, Router } from "@angular/router";
+import { Activity } from "src/app/models/Activity.model";
+import { ActivityService } from "../../services/Activity.service";
+import { LoginService } from "../../services/Login.service";
+
+
 
 @Component({
     selector:'schedule',
@@ -7,5 +13,18 @@ import { Component } from "@angular/core";
     styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent {
+
+  activity: Activity | undefined;
+
+    constructor(private router: Router, activatedRoute: ActivatedRoute, public service: ActivityService,
+        public loginService: LoginService) {
+
+        const id = activatedRoute.snapshot.params['id'];
+        service.getActivity(id).subscribe(
+            activity => this.activity = activity as Activity,
+            error => console.error(error),
+        );
+    }
+
 
 }
