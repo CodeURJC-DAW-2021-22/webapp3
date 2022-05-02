ab944f5de9d29467a274714b5a6a469e046666fa
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2022-05-02 10:10:13 +0200

    app routing and app module

--- frontend/rukygym/src/app/app.module.ts
+++ frontend/rukygym/src/app/app.module.ts
@@ -1,10 +1,10 @@
+
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { routing } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
-
 
 import { FooterComponent } from './components/footer/footer.component';
 import { AppComponent } from './app.component';
@@ -14,6 +14,12 @@ import { ContentComponent } from './components/content/content.component';
 import { MonitorActivities } from './components/monitorActivities/monitorActivities.component';
 import { ActivityInfo } from './components/activityInfo/activityInfo.component';
 import { MonitorSchedule } from './components/monitorSchedule/monitorSchedule.component';
+import { MonitorProfile } from './components/monitorProfile/monitorProfile.component';
+import { MonitorEditProfile } from './components/monitorEditProfile/monitorEditProfile.component';
+import { MonitorExerciseTable } from './components/monitorExerciseTable/monitorExerciseTable.component';
+import { MonitorAddExerciseTable } from './components/monitorAddExerciseTable/monitorAddExerciseTable.component';
+import { MonitorEditExerciseTable } from './components/monitorEditExerciseTable/monitorEditExerciseTable.component';
+import { MonitorExerciseTableInfo } from './components/monitorExerciseTableInfo/monitorExerciseTableInfo.component';
 
 
 @NgModule({
@@ -26,6 +32,12 @@ import { MonitorSchedule } from './components/monitorSchedule/monitorSchedule.co
     MonitorActivities,
     ActivityInfo,
     MonitorSchedule,
+    MonitorProfile,
+    MonitorEditProfile,
+    MonitorExerciseTable,
+    MonitorExerciseTableInfo,
+    MonitorEditExerciseTable,
+    MonitorAddExerciseTable,
   ],
   imports: [
     BrowserModule,
--- frontend/rukygym/src/app/app.routing.ts
+++ frontend/rukygym/src/app/app.routing.ts
@@ -1,3 +1,5 @@
+import { MonitorAddExerciseTable } from './components/monitorAddExerciseTable/monitorAddExerciseTable.component';
+import { MonitorExerciseTableInfo } from './components/monitorExerciseTableInfo/monitorExerciseTableInfo.component';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -8,6 +10,10 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ActivityInfo } from './components/activityInfo/activityInfo.component';
 import { MonitorActivities } from './components/monitorActivities/monitorActivities.component';
 import { MonitorSchedule } from './components/monitorSchedule/monitorSchedule.component';
+import { MonitorProfile } from './components/monitorProfile/monitorProfile.component';
+import { MonitorEditProfile } from './components/monitorEditProfile/monitorEditProfile.component';
+import { MonitorExerciseTable } from './components/monitorExerciseTable/monitorExerciseTable.component';
+import { MonitorEditExerciseTable } from './components/monitorEditExerciseTable/monitorEditExerciseTable.component';
 
 
 
@@ -15,6 +21,13 @@ const appRoutes = [
     {path: 'new/activities', component: MonitorActivities},
     {path: 'new/activity/:id', component: ActivityInfo},
     {path: 'new/schedule', component: MonitorSchedule},
+    {path: 'new/profile', component: MonitorProfile},
+    {path: 'new/monitorEditProfile', component: MonitorEditProfile},
+    {path: 'new/monitorExerciseTable', component : MonitorExerciseTable},
+    {path: 'new/monitorExerciseTableInfo/:id', component: MonitorExerciseTableInfo},
+    {path: 'new/monitorEditExerciseTable/:id', component: MonitorEditExerciseTable},
+    {path: 'new/monitorAddExerciseTable', component: MonitorAddExerciseTable},
+
 ]
 
 export const routing = RouterModule.forRoot(appRoutes);
