baf47dfe3075d3ca45a6a57d480a8e683658b728
fsarabiar <f.sarabia.2019@alumnos.urjc.es>
2022-05-02 10:11:18 +0200

    monitor see profile and edit profile

---
+++ frontend/rukygym/src/app/components/monitorProfile/monitorProfile.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'monitorProfile',
+  templateUrl: './monitorProfile.component.html',
+  styleUrls: ['./monitorProfile.component.css']
+})
+export class MonitorProfile {
+
+}
