686630cb44a3da6a969e6cc850a9b357af1513df
AndyNuzzi <andy.nuzzi@gmail.com>
2022-05-02 12:16:24 +0200

    information to edit profile

--- frontend/rukygym/src/app/components/edit-profile/edit-profile.component.ts
+++ frontend/rukygym/src/app/components/edit-profile/edit-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from "@angular/router";
 import { LoginService } from "src/app/services/Login.service";
+import { UserService } from 'src/app/services/User.service';
+import { User } from 'src/app/models/User.model';
 
 @Component({
   selector: 'editprofile',
@@ -9,13 +11,25 @@ import { LoginService } from "src/app/services/Login.service";
 })
 export class EditProfileComponent {
 
-  constructor(private router: Router, activatedRoute: ActivatedRoute,
+    member: User | undefined;
+    name: string = " ";
+
+    constructor(private router: Router, activatedRoute: ActivatedRoute, public service: UserService,
     public loginService: LoginService) {
 
+      const id = activatedRoute.snapshot.params['id'];
+      service.getMember(id).subscribe(
+      (member) => this.member = member as User,
+      (error: any)    => console.error(error)
+    );
 
+    this.name = this.member?.name as string;
 
     }
 
-
+    save(){
+      this.loginService.logIn("suuu@gmail.com", "password");
+      this.service.updateMonitor(this.member as User);
+    }
 
 }
