b28ba4e7fa3f896048f1969d738d70eeb51bd7a7
AlessandroNuzziURJC <80722183+AlessandroNuzziURJC@users.noreply.github.com>
2022-05-08 01:41:01 +0200

    Docker modifications

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -6,8 +6,20 @@ then
 	exit 1;
 fi
 
+#Directory change from /docker to /frontend/rukygym
+cd ../frontend/rukygym
+
+#Install node modules
+npm install
+
+#Build project
+ng build --prod --base-href="/new/"
+
+#Copy to src/main/resources/public/new
+cp -r dist/rukygym/* ../../backend/src/main/resources/public/new
+
 #Directory change from /docker to /backend
-cd ../backend
+cd ../../backend
 
 #Verify directory change
 echo "Directorio actual cambiado a $PWD"
